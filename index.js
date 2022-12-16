8c7ccf9c2c6e9146b9aa814ca7f3cf47b32c28ef
petersamson-kingori <peters.kingori@student.moringaschool.com>
2022-12-16 13:03:26 +0300

    Completed

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-const cats = ["Milo", "Otis", "Garfield"];
-cats;
+/////////fuction declarations
 
-console.log (cats.length);
+const cats = ["Milo", "Otis", "Garfield"];
+function Arrayfunction(){
+    beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+}
+
+  function destructivelyAppendCat(){
+    cats.push("Ralph");
+    return cats
+  }
+
+  function destructivelyPrependCat(){
+    cats.unshift("Bob");
+    return cats
+  }
+
+  function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats
+  }
+
+  function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats
+  }
+
+  function appendCat(){
+    let newCats = [...cats,"Broom"]
+    return newCats
+  }
+
+  function prependCat(){
+    let newCats = ["Arnold",...cats,]
+    return newCats
+  }
+  
+  function removeLastCat(){
+    let newCats = cats.slice(0,2);
+    return newCats
+  }
+
+  function removeFirstCat(){
+    let newCats = cats.slice(1);
+    return newCats
+  }
+
+
+  //////////////Function invocations.
+  Arrayfunction();
+  destructivelyAppendCat();
+  destructivelyPrependCat();
+  destructivelyRemoveLastCat();
+  console.log(cats)
+  
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -20,7 +20,7 @@ describe('index.js', function () {
       });
     });
 
-    describe('destructivelyPrependCat(name)', function () {
+    describe('(name)', function () {
       it('prepends a cat to the beginning of the cats array', function () {
         destructivelyPrependCat("Bob");
 
